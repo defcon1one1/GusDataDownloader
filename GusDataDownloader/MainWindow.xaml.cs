9a830eb88379008293e4b38adc247684ea51bee5
Jan Bzdyl <94219390+defcon1one1@users.noreply.github.com>
2023-05-18 21:40:08 +0200

    init

---
+++ GusDataDownloader/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using GusDataDownloader.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GusDataDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Area> AllAreas { get; set; }
+        public List<Area> DisplayedAreas { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            AllAreas = await DownloadAreas();
+            DataContext = this;
+
+            int defaultPageNumber = 1;
+            int defaultRecordsPerPage = 10;
+            UpdateDisplayedAreas(defaultPageNumber, defaultRecordsPerPage);
+        }
+
+
+        private async Task<List<Area>> DownloadAreas()
+        {
+            string url = "https://api-dbw.stat.gov.pl/api/1.1.0/area/area-area";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Area>>(json).OrderBy(a => a.Id).ToList(); // sort by id
+            }
+        }
+
+        private void UpdateDisplayedAreas(int pageNumber, int recordsPerPage)
+        {
+            int startIndex = (pageNumber - 1) * recordsPerPage;
+            DisplayedAreas = AllAreas.GetRange(startIndex, recordsPerPage);
+
+            OnPropertyChanged(nameof(DisplayedAreas));
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int pageNumber = Int32.Parse(pageNumberTextBox.Text);
+                int records = Int32.Parse(recordsPerPageComboBox.Text);
+                UpdateDisplayedAreas(pageNumber, records);
+            }
+            catch (FormatException formatException)
+            {
+                MessageBox.Show("Wrong page number format");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ GusDataDownloader/Models/Area.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace GusDataDownloader.Models
+{
+    public class Area
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("nazwa")]
+        public string Nazwa { get; set; }
+
+        [JsonProperty("id-nadrzedny-element")]
+        public int IdNadrzednyElement { get; set; }
+
+        [JsonProperty("id-poziom")]
+        public int IdPoziom { get; set; }
+
+        [JsonProperty("nazwa-poziom")]
+        public string NazwaPoziom { get; set; }
+
+        [JsonProperty("czy-zmienne")]
+        public bool CzyZmienne { get; set; }
+    }
+
+
+}
